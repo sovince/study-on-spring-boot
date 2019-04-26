5183803977e4b32578fe9a18792f104ab3e924b9
Vince <so_vince@outlook.com>
2019-04-26 21:24:17 +0800

    add scala env and jpa to boot

---
+++ src/main/scala/com/sovince/controller/ScalaController.scala
@@ -0,0 +1,20 @@
+package com.sovince.controller
+
+import org.springframework.web.bind.annotation.{GetMapping, RequestMapping, RestController}
+
+/**
+  * Created by vince
+  * Email: so_vince@outlook.com
+  * Data: 2019/4/26
+  * Time: 20:55
+  * Description:
+  */
+@RestController
+@RequestMapping(Array("/scala"))
+class ScalaController {
+
+  @GetMapping(Array("/hello"))
+  def hello(): String ={
+    "Hello Scala Boot!"
+  }
+}
