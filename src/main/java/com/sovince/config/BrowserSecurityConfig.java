908f42541eb7809d7641571ad4227c004a882ee5
Vince <so_vince@outlook.com>
2019-04-26 20:49:32 +0800

    update

--- src/main/java/com/sovince/config/BrowserSecurityConfig.java
+++ src/main/java/com/sovince/config/BrowserSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
 /**
  * Created by vince
  * Email: so_vince@outlook.com
@@ -16,9 +17,14 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //        super.configure(http);
-        http.formLogin().and()
+        http
+                //对德鲁伊关闭csrf检测
+                .csrf().disable()
+                //使用表单验证
+                .formLogin()
+                .and()
                 .authorizeRequests()
-                .antMatchers("/NotUsefulSecurity")
-                .authenticated();
+                .antMatchers("/", "/druid/*").permitAll()
+                .antMatchers("/NotUsefulSecurity").authenticated();
     }
 }
